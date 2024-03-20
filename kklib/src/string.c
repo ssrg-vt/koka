5367a6da4b784e9f815ccc55c0718dc8d22dc85c
swarnpriya <priya13swarn@gmail.com>
2024-03-19 22:43:59 -0400

    .

--- kklib/src/string.c
+++ kklib/src/string.c
@@ -986,13 +986,28 @@ kk_string_t kk_show_any(kk_box_t b, kk_context_t* ctx) {
 }
 
 // eBPF
-kk_unit_t kk_bpf_println(kk_string_t s, kk_context_t* ctx) {
+/*kk_unit_t kk_bpf_println(kk_string_t s, kk_context_t* ctx) {
   // TODO: set locale to utf-8?
-  puts(kk_string_cbuf_borrow(s, NULL, ctx));  // todo: allow printing embedded 0 characters?
-  //bpf_printk(kk_string_cbuf_borrow(s, NULL, ctx));
-  //FILE * fp; 
-  //fp = open ("/sys/kernel/debug/tracing/tacepipe", "w+");
-  //fprintf(fp, kk_string_cbuf_borrow(s, NULL, ctx)); // prints in the "/sys/kernel/debug/tracing/tacepipe" 
+  // puts(kk_string_cbuf_borrow(s, NULL, ctx));  // todo: allow printing embedded 0 characters?
+  // bpf_printk(s);
+  FILE * fp; 
+  fp = open ("/sys/kernel/debug/tracing/tracepipe", "a+");
+  if (fp) {
+    fprintf(fp, kk_string_cbuf_borrow(s, NULL, ctx)); // prints in the "/sys/kernel/debug/tracing/tacepipe" 
+    printf("I was able to open the tracepipe\n");
+  } {
+    printf("I was not able to write to tracepipe\n");
+  }
+  
+  kk_string_drop(s, ctx);
+  return kk_Unit;
+}*/
+
+
+// eBPF
+kk_unit_t kk_bpf_println(kk_string_t s, kk_context_t* ctx) {
+  char ____fmt[] = "Hello from koka";
+  bpf_trace_printk(____fmt, sizeof(____fmt));
   kk_string_drop(s, ctx);
   return kk_Unit;
 }
